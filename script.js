20d787f8dd8655a7652cadc8c276112225575568
SAWDAZAHGEER <149009541+SAWDAZAHGEER@users.noreply.github.com>
2024-01-21 19:58:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function removeLastCharacter() {
-    var inputValue = form1.i.value;
-    if (inputValue.length !== 0) {
-        form1.i.value = inputValue.substring(0, inputValue.length - 1);
-    }
-}
